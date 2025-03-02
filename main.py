9e300cde750e7498465c4ec459800f83b79c17fb
rushi1222 <k.rushi1222@gmail.com>
2025-03-02 09:33:06 -0500

    3rd

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ IS_DOCKER = os.path.exists("/.dockerenv")
 
 # ✅ Configure ChromeOptions
 options = Options()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-blink-features=AutomationControlled")
